c0d2a0f7f8dec125d40df5cdaa272d1000d6191b
kotru21 <arsenijbaturo2000@gmail.com>
2025-07-20 22:44:29 +0300

    select

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 // Export all components
 export { default as Button } from './components/Button/Button';
 export { default as TextField } from './components/TextField/TextField';
+export { Select } from './components/Select';
 
 // Export types
 export type { ButtonProps } from './components/Button/Button.types';
 export type { TextFieldProps } from './components/TextField/TextField.types';
+export type {
+  SelectProps,
+  SelectOption,
+} from './components/Select/Select.types';
